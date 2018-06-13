75d5dbfe6c4936a8e1e321ad3302f6c1bdc8f5a6
Juan Añez <janez@emida.net>
2018-06-13 09:42:31 -0500

    update files.

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,31 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">  -->
-    <div class="container-fluid">
-    <h1>{{ msg }}</h1>
-    <div class="row">
-    <div class="col-md-2">
-    <div class="list-group">
-        <a href="#" class="list-group-item active">
-          Cras justo odio
-        </a>
-        <router-link to="/home" class="list-group-item">Home</router-link>
-        <router-link to="/featured" class="list-group-item">Featured</router-link>
-        <router-link to="/restaurants"class="list-group-item" >Restaurants</router-link>
-        <!--<router-link :to="{ name: 'featured-id', params: {id: 16}}" class="list-group-item">Select</router-link>-->
-        <router-link to="/contact" class="list-group-item">Contact</router-link>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    </div>
-      <div class="col-md-10">
-        <div class="panel panel-default">
-          <div class="panel-heading">Panel heading without title</div>
-          <div class="panel-body">
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--
-    <home></home>
-    <contact></contact>
-    -->
-</div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome Restaurant App'
-    }
-  }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,32 +1,11 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import './plugins/axios'
 import App from './App.vue'
-import Home from './home.vue'
-import Contact from './contact.vue'
-import Featured from './destacados.vue'
-import Restaurants from './restaurants.vue'
+import router from './router'
 
-Vue.use(VueRouter);
-
-const routes = [
-  {path: '/featured-id/:id', name: '/featured-id', component: Featured},
-  {path: '/featured', component: Featured},
-  {path: '/restaurants', component: Restaurants},
-  {path: '/contact', component: Contact},
-  {path: '/home', component: Home},
-  {path: '/', component: Home}
-]
-
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-})
-
-Vue.component('home', Home);
-Vue.component('contact', Contact)
+Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
   router,
   render: h => h(App)
-})
+}).$mount('#app')
